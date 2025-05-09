dff31a4d400c3f995e5cfbee46f31bdc287d53e9
Alison Soares <alison.engfis@gmail.com>
2025-05-09 01:23:25 -0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -333,7 +333,7 @@ def plotar_grafico(url, estacoes_info, dados_filtrados, estacao_selecionada, cot
         legend=dict(
             orientation='h',
             yanchor='bottom',
-            y=1.09,
+            y=1.15,
             xanchor='left',
             x=0.01,
             font=dict(size=11),
